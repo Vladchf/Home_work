9ac95b862308ee9c0e44839dcf26844dbf7466c4
nemo <chf@mail.ru>
2024-11-17 13:21:13 +0300

    17 ноября

---
+++ Add_practical_task_on_Basic_data_structures.py
@@ -0,0 +1,18 @@
+# Дано Задание "Средний балл":
+# Вам необходимо решить задачу из реальной жизни: "школьные учителя устали подсчитывать вручную средний балл каждого ученика,
+# поэтому вам предстоит автоматизировать этот процесс":
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+print(grades)
+print(type(grades))
+print(grades[0])
+print(type(grades[0]))
+
+print('_______________')
+
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+print(students)
+print(type(students))
+
+# average_score={'Aaron': 4}
+# average_score=['Aaron']= 4
+# print(average_score)
