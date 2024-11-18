1e0b2d548509b55c0c56ca9f904fd1ee907370ae
nemo <chf@mail.ru>
2024-11-18 07:59:30 +0300

    17 ноября

--- Add_practical_task_on_Basic_data_structures.py
+++ Add_practical_task_on_Basic_data_structures.py
@@ -2,17 +2,67 @@
 # Вам необходимо решить задачу из реальной жизни: "школьные учителя устали подсчитывать вручную средний балл каждого ученика,
 # поэтому вам предстоит автоматизировать этот процесс":
 grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
-print(grades)
-print(type(grades))
-print(grades[0])
-print(type(grades[0]))
+# print(grades, 'оценки всех учащихся')
+# print(type(grades))
+print('_______________')
+
+# print(grades[0], 'Aaron оценки')
+# print(type(grades[0]))
+A = grades[0]
+# print(A)
+print('_______________')
+average_A = sum(A) / len(A)
+print(average_A, 'Aaron средний балл')
+print('_______________')
+
+# print(type(average_A))
+
+B = grades[1]
+print('_______________')
+average_B = sum(B) / len(B)
+print(average_B, 'Bilbo средний балл')
+print('_______________')
+
+J = grades[2]
+print('_______________')
+average_J = sum(J) / len(J)
+print(average_J, 'Johhny средний балл')
+print('_______________')
+
+K = grades[3]
+print('_______________')
+average_K = sum(K) / len(K)
+print(average_K, 'Khendrik средний балл')
+print('_______________')
+
+S = grades[4]
+print('_______________')
+average_S = sum(S) / len(S)
+print(average_S, 'Steve средний балл')
+print('_______________')
+
+
+#print(A)
+#print(B)
+#print(J)
+#print(K)
+#print(S)
 
 print('_______________')
 
-students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
-print(students)
-print(type(students))
+# print(type(a))
+# print(a[0])
+# print(type(a[0]))
 
-# average_score={'Aaron': 4}
-# average_score=['Aaron']= 4
-# print(average_score)
+
+
+print('_______________')
+
+#students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+#print(students)
+#print(type(students))
+
+Average_score = {}
+print(Average_score)
+Average_score['Aaron'] = average_A
+print(Average_score)
