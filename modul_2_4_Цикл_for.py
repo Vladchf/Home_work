07b29bfb7f0c8e5030466da0cd3dd87a7df38169
nemo <chf@mail.ru>
2025-01-29 22:43:04 +0300

    Четвертое звдание 2го модуля abyfk

--- modul_2_4_Цикл_for.py
+++ modul_2_4_Цикл_for.py
@@ -1,19 +1,19 @@
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
-primes = [] # Создал список простые числа
-not_primes = [] # Список "не простые числа"
+primes = []  # Создал список простые числа
+not_primes = []  # Список "не простые числа"
 for number in numbers:
-#    print(number)
+    #    print(number)
     if number == 1:
         continue
     is_prime = True
-    for div in range (2, number):
+    for div in range(2, number):
         if number % div == 0:
             not_primes.append(number)
             is_prime = False
             break
-        print(f' для числа {number} подбираем делитель {div}')
+        # print(f' для числа {number} подбираем делитель {div}')
     if is_prime:
         primes.append(number)
-print(numbers)
+# print(numbers)
 print(primes)
 print(not_primes)
